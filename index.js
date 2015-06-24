20fdf8bdd10d8e0657792f35dc86518d6c04b72a
Alex Gulakov <alexgulakov@gmail.com>
2015-06-24 03:06:50 +0000

    .

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
-var express = require('express'),
-app = express(),
-server = require('http').createServer(app),
+var express = require('express'), app = express(),
 bodyParser = require('body-parser'),
 cookieParser = require('cookie-parser'),
 session = require('express-session'),
-fs = require('fs'),
+mongoose = require('mongoose'),
+server = require('http').createServer(app).listen(80, function(){
+	console.log("SERVER STARTED " + (new Date().toLocaleString()));
+}),
 io = require('socket.io')(server);
 
-
-//db signin
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/debatedata', {
-  server: { poolSize: 1 }
-});
-
+//database
+mongoose.connect('mongodb://localhost/debatedata', {server: { poolSize: 1 }});
 
 //middleware
 app.use(cookieParser());
@@ -25,48 +21,19 @@ app.use(session({
     saveUninitialized: true
 }));
 
-
-//ROUTES*****************
-
 //auth
-app.use(require('./server/auth') );
-
+app.use(require('./server/auth'));
 function auth(req, res, next) {
   if (!req.isAuthenticated())
-    res.send("Login required");
-
+    return res.send("Login required");
   return next();
 }
 
+//routes
 app.use('/user', require('./server/user'));
 app.use('/doc', auth, require('./server/doc'));
 app.use('/round', auth, require('./server/round')(io));
 app.use('/', require('./server/misc'));
 
-
-
 //static routes
 app.use(express.static(__dirname + '/public'));
-
-
-//errors
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.send("<pre>"+(app.get('env') === 'development'? err.message : err.message) +"</pre>");
-});
-
-//run server
-server.listen(80);
-console.log("SERVER STARTED " + (new Date().toLocaleString()));
-
-
-
-
-
-
-
